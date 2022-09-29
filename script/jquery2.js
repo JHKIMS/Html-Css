fbc8cc8fd55b69ef297177b7be05144fe19ffede
JHKIMS <wjdgns5131@gmail.com>
2022-09-29 22:31:27 +0900

    Html,Css_심심풀이14:: Jquery2

---
+++ script/jquery2.js
@@ -0,0 +1,12 @@
+$(".menu li").mouseenter(function () {
+  $(this).children(".sub-menu").stop().slideDown();
+});
+
+$(".menu li").mouseleave(function () {
+  $(this).children(".sub-menu").stop().slideUp();
+});
+
+$(".btn span").click(function () {
+  $(this).addClass("active");
+  $(this).siblings().removeClass("active");
+});
